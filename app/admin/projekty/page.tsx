eec1d942d872683f5c307617675e2b3d7839b034
unknown <mgombr@gmail.com>
2026-04-27 18:55:50 +0200

    fix admin projekty module

---
+++ app/admin/projekty/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminProjekty() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Projekty</h1>
+      <p>Tu budeš upravovať projekty.</p>
+    </div>
+  );
+}
